dff617ecb110114608b003cc305f98613941eef2
mloshchilov <misha.loshilov1@gmail.com>
2021-09-14 15:21:17 +0300

    latest version

---
+++ js/script.js
@@ -0,0 +1,32 @@
+import tabs from './modules/tabs';
+import modal from './modules/modal';
+import timer from './modules/timer';
+import cards from './modules/cards';
+import slider from './modules/slider';
+import forms from './modules/forms';
+import calc from './modules/calc';
+import {openModal} from './modules/modal';
+
+window.addEventListener('DOMContentLoaded', function() {
+    const modalTimerId = setTimeout(() => openModal('.modal', modalTimerId), 30000);
+
+    tabs('.tabheader__item', '.tabcontent', '.tabheader__items', 'tabheader__item_active');
+    modal('[data-modal]', '.modal', modalTimerId);
+    timer('.timer', '2021-10-01');
+    cards();
+    slider({
+        container: '.offer__slider',
+        nextArrow: '.offer__slider-next',
+        prevArrow: '.offer__slider-prev',
+        totalCounter: '#total',
+        currentCounter: '#current',
+        wrapper: '.offer__slider-wrapper',
+        field: '.offer__slider-inner',
+        slide: '.offer__slide'
+    });
+    forms('form', modalTimerId);
+    calc();
+
+});
+
+
